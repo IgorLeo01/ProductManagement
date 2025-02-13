c250d733296897b878f56709855d98bcd9b1443c
IgorLeo01 <igor.mourasilva@upe.br>
2025-02-13 15:20:00 -0300

    Chore: manutention in program start

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using ProductManagementApp.Data;
+using ProductManagementApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ProductService>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
